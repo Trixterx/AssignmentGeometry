872d24e35fba9ed05c960f035c211f8c1e97d185
Dennis Lindquist <dennislindquist@gmail.com>
2021-04-20 07:42:56 +0200

    Add project files.

---
+++ AssignmentGeometri/GeometricCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentGeometri
+{
+    public class GeometricCalculator : GeometricThing
+    {
+        public float GetArea(GeometricThing thing)
+        {
+            return 0;
+        }
+        public float GetPerimeter(GeometricThing thing)
+        {
+            return 0;
+        }
+        public float GetPerimeter(GeometricThing[] thing)
+        {
+            return 0;
+        }
+    }
+}
