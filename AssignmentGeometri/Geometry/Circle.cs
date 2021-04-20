932966847c53d41b6140e333d31e9a7c70ae4ec2
Dennis Lindquist <dennislindquist@gmail.com>
2021-04-20 09:46:53 +0200

    1234

---
+++ AssignmentGeometri/Geometry/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentGeometri.Geometry
+{
+    public class Circle : GeometricThing
+    {
+        public float Radius { get; set; }
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+
+        public override float GetArea()
+        {
+            return 0;
+            //return (float)Math.Pow(side, 2)
+        }
+
+        public override float GetPerimeter()
+        {
+            return 0;
+            //return 4 * side;
+        }
+    }
+}
---
+++ AssignmentGeometri/Geometry/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentGeometri.Geometry
+{
+    public class Rectangle : GeometricThing
+    {
+        public float Side1 { get; set; }
+        public float Side2 { get; set; }
+
+        public Rectangle(int side1, int side2)
+        {
+            this.Side1 = side1;
+            this.Side2 = side2;
+        }
+
+        public override float GetArea()
+        {
+            return 0;
+            //return (float)Math.Pow(side, 2)
+        }
+
+        public override float GetPerimeter()
+        {
+            return 0;
+            //return 4 * side;
+        }
+    }
+}
